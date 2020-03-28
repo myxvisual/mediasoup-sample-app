39fcb415072b7e6a338a2d0916e3394bd7508584
myxvisual <myxvisual@live.com>
2020-03-28 10:05:23 +0800

    style: Change the code struct

--- server.js
+++ server.js
@@ -1,15 +1,12 @@
 const mediasoup = require('mediasoup');
-const fs = require('fs');
-const https = require('https');
-const express = require('express');
 const socketIO = require('socket.io');
 const config = require('./config');
+const { runStaticServer } = require('./staticServer')
 
 // Global variables
 let worker;
 let webServer;
 let socketServer;
-let expressApp;
 let producer;
 let consumer;
 let producerTransport;
@@ -18,8 +15,7 @@ let mediasoupRouter;
 
 (async () => {
   try {
-    await runExpressApp();
-    await runWebServer();
+    webServer = await runStaticServer();
     await runSocketServer();
     await runMediasoupWorker();
   } catch (err) {
@@ -27,51 +23,7 @@ let mediasoupRouter;
   }
 })();
 
-async function runExpressApp() {
-  expressApp = express();
-  expressApp.use(express.json());
-  expressApp.use(express.static(__dirname));
 
-  expressApp.use((error, req, res, next) => {
-    if (error) {
-      console.warn('Express app error,', error.message);
-
-      error.status = error.status || (error.name === 'TypeError' ? 400 : 500);
-
-      res.statusMessage = error.message;
-      res.status(error.status).send(String(error));
-    } else {
-      next();
-    }
-  });
-}
-
-async function runWebServer() {
-  const { sslKey, sslCrt } = config;
-  if (!fs.existsSync(sslKey) || !fs.existsSync(sslCrt)) {
-    console.error('SSL files are not found. check your config.js file');
-    process.exit(0);
-  }
-  const tls = {
-    cert: fs.readFileSync(sslCrt),
-    key: fs.readFileSync(sslKey),
-  };
-  webServer = https.createServer(tls, expressApp);
-  webServer.on('error', (err) => {
-    console.error('starting web server failed:', err.message);
-  });
-
-  await new Promise((resolve) => {
-    const { listenIp, listenPort } = config;
-    webServer.listen(listenPort, listenIp, () => {
-      const listenIps = config.mediasoup.webRtcTransport.listenIps[0];
-      const ip = listenIps.announcedIp || listenIps.ip;
-      console.log('server is running');
-      console.log(`open https://${ip}:${listenPort} in your web browser`);
-      resolve();
-    });
-  });
-}
 
 async function runSocketServer() {
   socketServer = socketIO(webServer, {
---
+++ staticServer.js
@@ -0,0 +1,71 @@
+const express = require('express');
+const https = require('https');
+const fs = require('fs');
+const config = require('./config');
+
+let expressApp;
+async function runExpressApp() {
+  expressApp = express();
+  expressApp.use(express.json());
+  expressApp.use(express.static(__dirname));
+
+  expressApp.use((error, req, res, next) => {
+    if (error) {
+      console.warn('Express app error,', error.message);
+
+      error.status = error.status || (error.name === 'TypeError' ? 400 : 500);
+
+      res.statusMessage = error.message;
+      res.status(error.status).send(String(error));
+    } else {
+      next();
+    }
+  });
+}
+
+let webServer;
+async function runWebServer() {
+  const {
+    sslKey,
+    sslCrt
+  } = config;
+  if (!fs.existsSync(sslKey) || !fs.existsSync(sslCrt)) {
+    console.error('SSL files are not found. check your config.js file');
+    process.exit(0);
+  }
+  const tls = {
+    cert: fs.readFileSync(sslCrt),
+    key: fs.readFileSync(sslKey),
+  };
+  webServer = https.createServer(tls, expressApp);
+  webServer.on('error', (err) => {
+    console.error('starting web server failed:', err.message);
+  });
+
+  await new Promise((resolve) => {
+    const {
+      listenIp,
+      listenPort
+    } = config;
+    webServer.listen(listenPort, listenIp, () => {
+      const listenIps = config.mediasoup.webRtcTransport.listenIps[0];
+      const ip = listenIps.announcedIp || listenIps.ip;
+      console.log('server is running');
+      console.log(`open https://${ip}:${listenPort} in your web browser`);
+      resolve();
+    });
+  });
+}
+
+async function runStaticServer() {
+  await runExpressApp();
+  await runWebServer();
+  return webServer
+}
+
+
+module.exports = {
+  runExpressApp,
+  runWebServer,
+  runStaticServer
+}
